191177d5c4806fb4837752aef540942086591172
Mary <76174247+maryhbb@users.noreply.github.com>
2023-10-19 15:19:58 +0200

    Implement handleSubmit and fetch category data

--- pages/form/index.js
+++ pages/form/index.js
@@ -1,7 +1,54 @@
 import ExpenseForm from "@/components/ExpenseForm";
+import Router from "next/router";
+import React from "react";
+import styled from "styled-components";
+import useSWR from "swr";
+import Link from "next/link";
 
 function Form() {
-  return <ExpenseForm />;
+  const { data, error, mutate } = useSWR(`/api/categories`);
+  const category = data;
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <h1> Error:{error.message} </h1>;
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formdata = new FormData(event.target);
+    const expData = Object.fromEntries(formdata);
+
+    const response = await fetch("/api/expenses", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(expData),
+    });
+
+    if (!response.ok) {
+      console.error(response.status);
+      return;
+    }
+
+    mutate();
+    event.target.reset();
+
+    Router.push("/");
+  };
+
+  return (
+    <ExpenseForm
+      onSubmit={handleSubmit}
+      isEditMode={true}
+      category={category}
+    />
+  );
 }
 
 export default Form;
