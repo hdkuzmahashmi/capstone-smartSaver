2d9b14e1ef478e8fcea488b80ab0e30c17b74720
Mary <76174247+maryhbb@users.noreply.github.com>
2023-11-03 14:50:46 +0100

    Isolate donut graph styling in separate file in the design-system

--- components/DoughnutGraph/index.jsx
+++ components/DoughnutGraph/index.jsx
@@ -1,87 +1,19 @@
-import styled from "styled-components";
 import { Doughnut } from "react-chartjs-2";
 import { Chart, ArcElement, Tooltip } from "chart.js";
 import useSWR from "swr";
 import { Icon } from "@iconify/react";
 import Loading from "../Loading";
+import {
+  GraphContainer,
+  TotalContainer,
+  ListContainer,
+  ListItem,
+  ColorBox,
+  ItemName,
+  Amount,
+} from "@/design-system/StyledDoughnutGraph";
 
 Chart.register(ArcElement, [Tooltip]);
-
-const GraphContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 3rem;
-  margin-top: 3rem;
-  gap: 5vh;
-  width: 100%;
-
-  box-shadow: inset 0 0 35px 5px rgba(0, 0, 0, 0.1),
-    inset 0 2px 1px 1px rgba(255, 255, 255, 0.9),
-    inset 0 -2px 1px rgba(0, 0, 0, 0.05);
-  border: 1px solid rgba(0, 0, 0, 0.2);
-  border-radius: 15px;
-  position: relative;
-`;
-
-const TotalContainer = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-  font-size: 20px;
-  font-weight: bold;
-
-  color: #333;
-
-  div:nth-child(2) {
-    font-size: 24px;
-    margin-top: 8px;
-    color: #ff6600;
-    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.4);
-  }
-`;
-
-const ListContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-`;
-
-const ListItem = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin: 5px;
-  width: 100%;
-  gap: 0.3rem;
-  border-radius: 15px;
-  padding: 2px;
-  padding-left: 13px;
-  font-size: 13px;
-`;
-
-const ColorBox = styled.div`
-  width: 15px;
-  height: 15px;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-right: 10px;
-`;
-
-const ItemName = styled.div`
-  flex: 1;
-  display: flex;
-  align-items: center;
-`;
-
-const Amount = styled.div`
-  white-space: nowrap;
-  margin-right: 5px;
-`;
 
 function DoughnutGraph() {
   const { data, error } = useSWR("/api/expenses");
@@ -167,10 +99,10 @@ function DoughnutGraph() {
       <ListContainer>
         {categoryTotalsArray.map((category, index) => (
           <ListItem key={index}>
+            <ColorBox style={{ backgroundColor: category.color }}></ColorBox>
             <Icon icon={category.icon} width={15} />
             <ItemName>{category.name}</ItemName>
             <Amount>{category.total} €</Amount>
-            <ColorBox style={{ backgroundColor: category.color }}></ColorBox>
           </ListItem>
         ))}
       </ListContainer>
---
+++ design-system/StyledDoughnutGraph.js
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+
+export const GraphContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 3rem;
+  margin-top: 3rem;
+  gap: 5vh;
+  width: 100%;
+  box-shadow: inset 0 0 35px 5px rgba(0, 0, 0, 0.1),
+    inset 0 2px 1px 1px rgba(255, 255, 255, 0.9),
+    inset 0 -2px 1px rgba(0, 0, 0, 0.05);
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 15px;
+  position: relative;
+
+  @media (max-width: 768px) {
+    padding: 1rem;
+    gap: 2vh;
+    flex-direction: column;
+  }
+`;
+
+export const TotalContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  color: #202020;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+`;
+
+export const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ListItem = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0.2rem;
+  width: 100%;
+  gap: 0.3rem;
+  font-size: 13px;
+`;
+
+export const ColorBox = styled.div`
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  margin-right: 0.3rem;
+`;
+
+export const ItemName = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+export const Amount = styled.div`
+  white-space: nowrap;
+  margin-right: 5px;
+`;
