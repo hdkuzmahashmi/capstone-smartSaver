78525af8f712ace4eeec52c3e1349e5391263bb5
Mary <76174247+maryhbb@users.noreply.github.com>
2023-11-17 18:40:55 +0100

    Implement getMonthlyExpenseSumsWithMonthNames func

--- components/BarChart/index.jsx
+++ components/BarChart/index.jsx
@@ -11,17 +11,75 @@ import {
 } from "chart.js";
 
 import { GraphContainer } from "@/design-system/StyledDoughnutGraph";
+import useSWR from "swr";
+import Loading from "../Loading";
 
-Chart.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+Chart.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 function BarChart() {
+  const { data, error } = useSWR("/api/expenses");
+
+  if (!data) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  console.log("data:", data);
+  const createdAt = data[0].createdAt;
+
+  console.log("date:", createdAt);
+  const createdAtValues = data.map((item) => item.createdAt);
+  console.log("data array:", createdAtValues);
+
+  ////////////////////
+
+  // Assuming data is your array of expense objects
+
+  function getMonthlyExpenseSumsWithMonthNames(data) {
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    // Create an array to store objects with month names and corresponding sums
+    const monthlySumsWithNames = Array.from({ length: 12 }, (_, monthIndex) => {
+      return { month: monthNames[monthIndex], sum: 0 };
+    });
+
+    // Iterate through the data and accumulate the sums for each month
+    data.forEach((item) => {
+      const createdAtDate = new Date(item.createdAt);
+      const monthIndex = createdAtDate.getMonth();
+      monthlySumsWithNames[monthIndex].sum += item.amount;
+    });
+
+    // Separate the array into two arrays for keys (month names) and values (sums)
+    const keysArray = monthlySumsWithNames.map((item) => item.month);
+    const valuesArray = monthlySumsWithNames.map((item) => item.sum);
+
+    return { keys: keysArray, values: valuesArray };
+  }
+
+  // Example usage
+  const { keys, values } = getMonthlyExpenseSumsWithMonthNames(data);
+  console.log("Keys (Month Names):", keys);
+  console.log("Values (Sum of Expenses):", values);
+
+  /////////////
+
   const options = {
     plugins: {
       legend: {
@@ -29,7 +87,7 @@ function BarChart() {
       },
       title: {
         display: true,
-        text: "Chart.js Bar Chart",
+        text: "Monthly Report",
       },
     },
     maintainAspectRatio: false,
