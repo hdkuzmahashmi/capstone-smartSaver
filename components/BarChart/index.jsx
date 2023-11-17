f7ee05cfc381d4585c3789962d01be6fabea0b9a
Mary <76174247+maryhbb@users.noreply.github.com>
2023-11-17 21:50:36 +0100

    Adjust getMonthlyExpenseSumsByMonth function

--- components/BarChart/index.jsx
+++ components/BarChart/index.jsx
@@ -38,7 +38,7 @@ function BarChart() {
 
   // Assuming data is your array of expense objects
 
-  function getMonthlyExpenseSumsWithMonthNames(data) {
+  function getMonthlyExpenseSumsByMonth(data) {
     const monthNames = [
       "January",
       "February",
@@ -54,31 +54,39 @@ function BarChart() {
       "December",
     ];
 
-    // Create an array to store objects with month names and corresponding sums
-    const monthlySumsWithNames = Array.from({ length: 12 }, (_, monthIndex) => {
-      return { month: monthNames[monthIndex], sum: 0 };
+    // Initialize an array to store objects with month names and corresponding sums
+    const monthlySumsByMonth = Array.from({ length: 12 }, (_, monthIndex) => {
+      return { month: monthNames[monthIndex], totalExpense: 0 };
     });
 
     // Iterate through the data and accumulate the sums for each month
-    data.forEach((item) => {
-      const createdAtDate = new Date(item.createdAt);
+    data.forEach((expense) => {
+      const createdAtDate = new Date(expense.createdAt);
       const monthIndex = createdAtDate.getMonth();
-      monthlySumsWithNames[monthIndex].sum += item.amount;
+      monthlySumsByMonth[monthIndex].totalExpense += expense.amount;
     });
 
     // Separate the array into two arrays for keys (month names) and values (sums)
-    const keysArray = monthlySumsWithNames.map((item) => item.month);
-    const valuesArray = monthlySumsWithNames.map((item) => item.sum);
+    const monthNamesArray = monthlySumsByMonth.map((item) => item.month);
+    const totalExpensesArray = monthlySumsByMonth.map(
+      (item) => item.totalExpense
+    );
 
-    return { keys: keysArray, values: valuesArray };
+    return { monthNames: monthNamesArray, totalExpenses: totalExpensesArray };
   }
 
   // Example usage
-  const { keys, values } = getMonthlyExpenseSumsWithMonthNames(data);
-  console.log("Keys (Month Names):", keys);
-  console.log("Values (Sum of Expenses):", values);
+
+  console.log(
+    "totalexpenses in every months:",
+    getMonthlyExpenseSumsByMonth(data)
+  );
+  const { monthNames, totalExpenses } = getMonthlyExpenseSumsByMonth(data);
+  console.log("Month Names:", monthNames);
+  console.log("Total Expenses:", totalExpenses);
 
   /////////////
+
 
   const options = {
     plugins: {
@@ -95,12 +103,40 @@ function BarChart() {
   };
 
   const chartData = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: monthNames,
     datasets: [
       {
         label: "Faght Test",
-        data: [10, 30, 50, 20, 40, 10, 50],
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
+        data: [78, 80, 66, 56, 161, 173, 166, 175, 150, 93, 131, 111],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(120, 255, 120, 0.2)",
+          "rgba(255, 0, 255, 0.2)",
+          "rgba(0, 255, 255, 0.2)",
+          "rgba(255, 128, 0, 0.2)",
+          "rgba(0, 255, 128, 0.2)",
+          "rgba(128, 0, 255, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+          "rgba(120, 255, 120, 1)",
+          "rgba(255, 0, 255, 1)",
+          "rgba(0, 255, 255, 1)",
+          "rgba(255, 128, 0, 1)",
+          "rgba(0, 255, 128, 1)",
+          "rgba(128, 0, 255, 1)",
+        ],
+        borderWidth: 1,
       },
     ],
   };
@@ -108,7 +144,7 @@ function BarChart() {
   return (
     <>
       <GraphContainer>
-        <Bar options={options} data={chartData} />
+        <Bar style={{ height: '300px' }} options={options} data={chartData} />
       </GraphContainer>
     </>
   );
