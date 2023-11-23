099d7c3a27aa6928f2817601b9c21f86c55938c6
Mary <76174247+maryhbb@users.noreply.github.com>
2023-11-23 18:01:27 +0100

    Implement FileUpload component

---
+++ components/FileUpload/index.jsx
@@ -0,0 +1,16 @@
+import { FileInput } from "@/design-system/StyledInput";
+import React, { useState } from "react";
+
+function FileUpload({ onFileChange }) {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+    onFileChange(selectedFile);
+  };
+
+  return <FileInput type="file" onChange={handleFileChange} accept="image/*" />;
+}
+
+export default FileUpload;
