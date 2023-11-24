c652a8517475821409901aaf01b9d74036fd6d67
Mary <76174247+maryhbb@users.noreply.github.com>
2023-11-24 11:57:41 +0100

    IMplement handleFileChange function

--- components/UploadFile/index.jsx
+++ components/UploadFile/index.jsx
@@ -2,15 +2,37 @@ import { FileInput } from "@/design-system/StyledInput";
 import React, { useState } from "react";
 
 function UploadFile({ onFileChange }) {
-  const [file, setFile] = useState(null);
+  const [file, setFile] = useState([]);
 
   const handleFileChange = (event) => {
-    const selectedFile = event.target.files[0];
-    setFile(selectedFile);
-    onFileChange(selectedFile);
+    const selectedFiles = event.target.files;
+    // setFile(selectedFile);
+    // onFileChange(selectedFile);
+
+    console.log("selectedFiles:", selectedFiles);
+
+    const newFiles = [...selectedFiles].filter((file) => {
+      console.log("newFiles:", file);
+
+      if (file.size < 1024 * 1024 && file.type.startsWith("image/")) {
+        return file;
+      }
+    });
+    console.log({ selectedFiles, newFiles });
+
+    setFile(prev=> [...newFiles, ...prev] )
+
+    set
   };
 
-  return <FileInput type="file" onChange={handleFileChange} accept="image/*" />;
+  return (
+    <FileInput
+      type="file"
+      onChange={handleFileChange}
+      accept="image/*"
+      multiple
+    />
+  );
 }
 
 export default UploadFile;
