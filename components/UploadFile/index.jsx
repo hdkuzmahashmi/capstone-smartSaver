66e2479b6dcf00cf6ceddaf16e21d5fd1f41e289
Mary <76174247+maryhbb@users.noreply.github.com>
2023-12-04 17:33:18 +0100

    Implement display and delete Receipts

--- components/UploadFile/index.jsx
+++ components/UploadFile/index.jsx
@@ -1,37 +1,58 @@
 import { FileInput } from "@/design-system/StyledInput";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import ReceiptDisplay from "../ReceiptDisplay";
+import styled from "styled-components";
+
+const ReceiptContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  margin: 10px;
+`;
 
 function UploadFile({ onFileChange }) {
-  const [file, setFile] = useState([]);
+  const [files, setFiles] = useState([]);
 
   const handleFileChange = (event) => {
     const selectedFiles = event.target.files;
-    // setFile(selectedFile);
-    // onFileChange(selectedFile);
 
     console.log("selectedFiles:", selectedFiles);
 
     const newFiles = [...selectedFiles].filter((file) => {
       console.log("newFiles:", file);
 
-      if (file.size < 1024 * 1024 && file.type.startsWith("image/")) {
-        return file;
-      }
+      // Only accept files less than 1mb and of type 'image/'
+      return file.size < 1024 * 1024 && file.type.startsWith("image/");
     });
+
     console.log({ selectedFiles, newFiles });
 
-    setFile(prev=> [...newFiles, ...prev] )
+    setFiles((prevFiles) => [...newFiles, ...prevFiles]);
+  };
 
-    set
+  const handleDeleteFile = (index) => {
+    setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
   };
 
   return (
-    <FileInput
-      type="file"
-      onChange={handleFileChange}
-      accept="image/*"
-      multiple
-    />
+    <>
+      <FileInput
+        type="file"
+        onChange={handleFileChange}
+        accept="image/*"
+        multiple
+      />
+
+      <ReceiptContainer>
+        {files.map((file, index) => (
+          <ReceiptDisplay
+            key={index}
+            url={URL.createObjectURL(file)}
+            onClick={() => handleDeleteFile(index)}
+          />
+        ))}
+      </ReceiptContainer>
+    </>
   );
 }
 
