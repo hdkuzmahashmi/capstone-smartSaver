c1bd383844ca59fb61b79bca26bc944dff524c5d
Mary <76174247+maryhbb@users.noreply.github.com>
2023-12-08 15:41:30 +0100

    Enable multiple file uploads in handleCloudinaryUpload

--- pages/api/upload/index.js
+++ pages/api/upload/index.js
@@ -1,36 +1,56 @@
 import formidable from "formidable";
 import cloudinary from "cloudinary";
 
+// Configure Cloudinary with API credentials
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
 });
 
+// Configuration for Next.js API route to disable default bodyParser
 export const config = {
   api: {
     bodyParser: false,
   },
 };
 
+// Handle Cloudinary image uploads
 export default async function handleCloudinaryUpload(request, response) {
   if (request.method !== "POST") {
-    console.log("test request");
     response.status(400).json({ message: "Method not allowed" });
     return;
   }
 
-  const form = formidable({});
+  try {
+    // Create a formidable form parser with support for multiple files
+    const form = formidable({ multiples: true });
 
-  const [fields, files] = await form.parse(request);
-  const file = files.file[0];
-  const { newFilename, filepath } = file;
-  const result = await cloudinary.v2.uploader.upload(filepath, {
-    public_id: newFilename,
-    folder: "Smartsaver",
-  });
+    // Parse the incoming request to extract form fields and files
+    const [fields, files] = await form.parse(request);
 
-  console.log("result from cloudinary: ", result);
+    // Extract the uploaded files from the form data
+    const uploadedFiles = files.file;
+    console.log("uploadedFiles:", uploadedFiles);
 
-  response.status(200).json(result);
+    // Use Promise.all to upload each file to Cloudinary concurrently
+    const uploadPromises = uploadedFiles.map(async (file) => {
+      const { newFilename, filepath } = file;
+
+      // Upload the file to Cloudinary and configure options
+      const result = await cloudinary.v2.uploader.upload(filepath, {
+        public_id: newFilename,
+        folder: "Smartsaver",
+      });
+
+      return result;
+    });
+
+    // Wait for all uploads to complete and send the results in the response
+    const results = await Promise.all(uploadPromises);
+    response.status(200).json(results);
+  } catch (error) {
+    console.error("Error processing files:", error);
+    response.status(500).json({ message: "Internal Server Error" });
+  }
 }
--- pages/create/index.jsx
+++ pages/create/index.jsx
@@ -3,63 +3,32 @@ import Router from "next/router";
 import React from "react";
 import { mutate } from "swr";
 
+// Constants for API endpoints
+const API_EXPENSES = "/api/expenses";
+const API_UPLOAD = "/api/upload";
+
 function CreatePage({ setToast }) {
+  // Handle the form submission
   const handleAdd = async (event) => {
     try {
       event.preventDefault();
 
+      // Extract form data
       const formdata = new FormData(event.target);
-      const expData = Object.fromEntries(formdata);
+
+      // Transform form data into a structured object
+      const expData = transformFormData(formdata);
+
+      // Call functions to add expense and upload image
+      await addExpense(expData);
+      await uploadImage(formdata);
+
+      console.log("formdata:", formdata);
       console.log("expData:", expData);
-
-      // Send the expense data to your expenses API
-      const response = await fetch("/api/expenses", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(expData),
-      });
-
-      if (!response.ok) {
-        console.error(response.status);
-        setToast(
-          true,
-          "Something went wrong, API does not respond with data. Please contact the application administrator.",
-          "warning"
-        );
-        return;
-      }
-
-      // Upload image to Cloudinary
-      try {
-        const cloudinaryResponse = await fetch("/api/upload", {
-          method: "POST",
-          body: formdata,
-        });
-
-        if (!cloudinaryResponse.ok) {
-          console.error(cloudinaryResponse.status);
-          setToast(
-            true,
-            "Error uploading image to Cloudinary. Please try again.",
-            "warning"
-          );
-          return;
-        }
-      } catch (error) {
-        console.error("Error uploading image:", error);
-        setToast(
-          true,
-          "Error uploading image to Cloudinary. Please try again.",
-          "warning"
-        );
-        return;
-      }
 
       // Reset form, trigger re-fetch, and navigate
       event.target.reset();
-      mutate("/api/expenses");
+      mutate(API_EXPENSES);
       Router.push("/details");
       setToast(true, "Expense added successfully!", "success");
     } catch (error) {
@@ -72,6 +41,72 @@ function CreatePage({ setToast }) {
     }
   };
 
+  // Function to transform FormData to a structured object
+  const transformFormData = (formdata) => {
+    const expData = {};
+    for (const [key, value] of formdata.entries()) {
+      if (expData[key]) {
+        // Check if the existing value is iterable
+        expData[key] = Array.isArray(expData[key])
+          ? [...expData[key], value]
+          : [expData[key], value];
+      } else {
+        // If the key does not exist, set the value
+        expData[key] = value;
+      }
+    }
+    return expData;
+  };
+
+  // Function to add an expense via API
+  const addExpense = async (expData) => {
+    const response = await fetch(API_EXPENSES, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(expData),
+    });
+
+    // Handle API response errors
+    if (!response.ok) {
+      handleApiError(response, "API does not respond with data.");
+    }
+  };
+
+  // Function to upload an image to Cloudinary
+  const uploadImage = async (formdata) => {
+    try {
+      const cloudinaryResponse = await fetch(API_UPLOAD, {
+        method: "POST",
+        body: formdata,
+      });
+
+      // Handle Cloudinary API response errors
+      if (!cloudinaryResponse.ok) {
+        handleApiError(
+          cloudinaryResponse,
+          "Error uploading image to Cloudinary. Please try again."
+        );
+      }
+    } catch (error) {
+      console.error("Error uploading image:", error);
+      setToast(
+        true,
+        "Error uploading image to Cloudinary. Please try again.",
+        "warning"
+      );
+    }
+  };
+
+  // Handle API response errors in a consistent manner
+  const handleApiError = (response, errorMessage) => {
+    console.error(response.status);
+    setToast(true, `Something went wrong. ${errorMessage}`, "warning");
+    throw new Error(`API Error: ${response.status}`);
+  };
+
+  // Render the form component
   return <Form onSubmit={handleAdd} isEditMode={false} setToast={setToast} />;
 }
 
