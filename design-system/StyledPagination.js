26b0dc16ff5db54b7dcc67afbf12291d8c8bc74a
Felicitas Kroder <Felicitas.Kroder@gmail.com>
2023-12-17 22:50:25 +0100

    Polish Pagination Styling

--- design-system/StyledPagination.js
+++ design-system/StyledPagination.js
@@ -14,14 +14,19 @@ export const PaginationContainer = styled.div`
 export const PaginationButton = styled.button`
   height: 42px;
   width: 42px;
-  background-color: ${({ $active }) => ($active ? "#56a2e8" : "gray")};
+  box-shadow: inset -0.13em -0.15em 0.2em rgba(0, 0, 0, 0.2);
+  background: ${({ $active }) =>
+    $active ? "linear-gradient(to right, #78b3e6, #1c91e3);" : "gray"};
   color: #fff;
   border: none;
   cursor: pointer;
   border-radius: 50%;
 
   &:hover {
-    background-color: #56a2e8;
+    /* border: 1px solid black; */
+    box-shadow: inset -0.1em -0.1em 0.15em rgba(0, 0, 0, 0.2),
+      inset 0.01em 0.03em 0.8em rgba(0, 0, 0, 0.3),
+      0.06em 0 0.4em rgba(0, 0, 0, 0.21);
   }
 
   &:focus {
